57eaa2c3df9d18e8f93623258c49a9b75150f6cb
William Mburu <williammburu79@gmail.com>
2024-07-18 07:55:48 +0300

    modified:   components/Footer.tsx 	new file:   constants/index.ts

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-
+import { footerLinks } from "@/constants";
 
 
 const Footer = () => {
@@ -9,6 +9,25 @@ const Footer = () => {
         <div className="flex max-md:flex-col flex-wrap justify-betweeen gap-5 sm:px-16 px-6 py-10">
             <div className="flex flex-col justify-start items-start gap-6">
                 <Image src="/logo.png" alt="logo" width={80} height={18} className="object-contain"/>
+                <p className="text-base text-gray-700">Cars Hub <br /> All rights reserved &copy; 2023</p>
+            </div>
+            <div className="footer__links">
+                {footerLinks.map((link) => (
+                    <div key={link.title} className="footer__link"> 
+                        <h3 className="font-bold">{link.title}</h3>
+                        {
+                            link.links.map((item) => (
+                                <Link 
+                                    key={item.title}
+                                    href={item.url}
+                                    className="text-gray-500"
+                                >
+                                    {item.title}
+                                </Link>
+                            ))
+                        }
+                    </div>
+                ))}
             </div>
         </div>
     </footer>
